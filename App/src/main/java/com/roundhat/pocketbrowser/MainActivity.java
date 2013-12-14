fe77408049ab63d814c60809c6e7673f1b9f2e4e
beila <beila@kaist.ac.kr>
2013-12-15 08:24:53 +0900

    become invisible

--- App/src/main/java/com/roundhat/pocketbrowser/MainActivity.java
+++ App/src/main/java/com/roundhat/pocketbrowser/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends Activity {
         intent.putExtra("com.pocket.reader.extra.internal.startSource", uriString);
         intent.putExtra("com.pocket.reader.extra.internal.itemUrl", uriString);
         intent.putExtra("com.pocket.reader.external.extra.url", uriString);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 
