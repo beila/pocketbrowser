d83baefee7c93a12340607772f342243a93000b7
beila <beila@kaist.ac.kr>
2013-12-15 08:48:14 +0900

    install Pocket if there's no Pocket

--- App/src/main/java/com/roundhat/pocketbrowser/MainActivity.java
+++ App/src/main/java/com/roundhat/pocketbrowser/MainActivity.java
@@ -3,13 +3,19 @@ package com.roundhat.pocketbrowser;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         final Uri data = getIntent().getData();
         if (null == data) {finish(); return;}
         String uriString = data.toString();
@@ -21,7 +27,24 @@ public class MainActivity extends Activity {
         intent.putExtra("com.pocket.reader.extra.internal.itemUrl", uriString);
         intent.putExtra("com.pocket.reader.external.extra.url", uriString);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        if (isPocketAvailable(intent)) startActivity(intent);
+        else finish();
+    }
+
+    private boolean isPocketAvailable(Intent intent) {
+        PackageManager packageManager = getPackageManager();
+        if (packageManager == null) return false;
+
+        List<ResolveInfo>  activities = packageManager.queryIntentActivities(intent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        if (!isIntentSafe) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id="+"com.ideashower.readitlater.pro"))
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            );
+        }
+        return isIntentSafe;
     }
 
 }
