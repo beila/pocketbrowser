3be8e0dbc1c4f5503b29de22f7c0d5da4ea172ee
beila <beila@kaist.ac.kr>
2013-12-15 08:16:12 +0900

    this is a real browser!

--- App/src/main/java/com/roundhat/pocketbrowser/MainActivity.java
+++ App/src/main/java/com/roundhat/pocketbrowser/MainActivity.java
@@ -3,22 +3,23 @@ package com.roundhat.pocketbrowser;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
-/**
- * Created by hojin.ghim on 13. 12. 14.
- */
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Uri data = getIntent().getData();
+        if (null == data) {finish(); return;}
+        String uriString = data.toString();
+
         final Intent intent = new Intent();
         intent.setComponent(new ComponentName("com.ideashower.readitlater.pro",
                 "com.ideashower.readitlater.activity.ReaderActivity"));
-//        intent.setData(Uri.parse("http://google.com/calendar"));
-        intent.putExtra("com.pocket.reader.extra.internal.startSource", "http://google.com/calendar");
-        intent.putExtra("com.pocket.reader.extra.internal.itemUrl", "http://google.com/calendar");
-        intent.putExtra("com.pocket.reader.external.extra.url", "http://google.com/calendar");
+        intent.putExtra("com.pocket.reader.extra.internal.startSource", uriString);
+        intent.putExtra("com.pocket.reader.extra.internal.itemUrl", uriString);
+        intent.putExtra("com.pocket.reader.external.extra.url", uriString);
         startActivity(intent);
     }
 
